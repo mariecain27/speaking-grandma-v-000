42f4fa3410189f31dd594e9aef4f650a5e286a71
Marie Cain <mariecain27@gmail.com>
2018-05-29 02:15:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,8 @@ def speak_to_grandma(phrase)
   
   if phrase.upcase
     'NO, NOT SINCE 1938!'
+  elsif phrase == 'I LOVE YOU GRANDMA!'
+    'I LOVE YOU TOO PUMPKIN!'
   else
     'HUH?! SPEAK UP, SONNY!'
   end
